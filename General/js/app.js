6271e9ca001397dd9953b2a2009f9f3701529d6e
Sergij <barsulsergij@gmail.com>
2022-10-13 14:26:12 -0700

    first commit

---
+++ General/js/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper3 = new Swiper(".mySwiper3", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  function myFunction() {
+    var element = document.getElementById("myBody");
+    element.classList.toggle("myBody");
+  }
